9ece1fe0d2fc39001f552ec2413d2977cead3d08
JRazek <jakub.jakubecky123@gmail.com>
2022-01-07 01:11:47 +0100

    disconnect in destructor

--- tizen/src/BluetoothDeviceController.cc
+++ tizen/src/BluetoothDeviceController.cc
@@ -12,15 +12,16 @@ namespace btu {
         std::scoped_lock lock(gattServerHandle.mut);
         auto& handle=gattServerHandle.var.first;
         const auto& isConnected=gattServerHandle.var.second;
+        if(isConnected) disconnect();
 
     }
 
     auto BluetoothDeviceController::address() noexcept -> decltype(_address)& { return _address; }
     auto BluetoothDeviceController::cAddress() const noexcept -> const decltype(_address)& { return _address; }
     auto BluetoothDeviceController::state() noexcept -> decltype(_state)& { return _state; }
-    auto BluetoothDeviceController::cState() const noexcept -> const decltype(_state)&{ return _state; }
-    auto BluetoothDeviceController::protoBluetoothDevices() noexcept -> decltype(_protoBluetoothDevices)&{ return _protoBluetoothDevices; }
-    auto BluetoothDeviceController::cProtoBluetoothDevices() const noexcept -> const decltype(_protoBluetoothDevices)&{ return _protoBluetoothDevices; }
+    auto BluetoothDeviceController::cState() const noexcept -> const decltype(_state)& { return _state; }
+    auto BluetoothDeviceController::protoBluetoothDevices() noexcept -> decltype(_protoBluetoothDevices)& { return _protoBluetoothDevices; }
+    auto BluetoothDeviceController::cProtoBluetoothDevices() const noexcept -> const decltype(_protoBluetoothDevices)& { return _protoBluetoothDevices; }
 
     auto BluetoothDeviceController::connect() -> void {
         std::scoped_lock lock(gattServerHandle.mut);
@@ -40,7 +41,7 @@ namespace btu {
         if(isConnected){
             int res=bt_gatt_service_destroy(handle);
             isConnected=false;
-            
+
         }else{
             Logger::log(LogLevel::ERROR, "cannot disconnect. Device not connected "+_address);
         }
