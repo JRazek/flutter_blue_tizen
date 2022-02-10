fd412fad40806aa9d67bdf00e3c508d69dad394b
JRazek <jakub.jakubecky123@gmail.com>
2022-02-10 22:55:31 +0100

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,7 +34,7 @@ void main() async {
     if (sc.advertisementData.localName == "Galaxy S20 FE JRazek") {
       await dev.connect(autoConnect: false);
       var services = await dev.discoverServices();
-      // await Future.delayed(const Duration(seconds: 20));
+      await Future.delayed(const Duration(seconds: 0));
       for (var service in services) {
         for (var characteristic in service.characteristics) {
           // if (characteristic.properties.read &&
@@ -55,13 +55,12 @@ void main() async {
           // }
           if ((characteristic.properties.notify ||
               characteristic.properties.indicate)) {
-            await characteristic.setNotifyValue(true);
-            await characteristic.setNotifyValue(false);
+            characteristic.setNotifyValue(true);
           }
         }
       }
       debugPrint("released");
-      // await dev.disconnect();
+      await dev.disconnect();
     }
   }
 
